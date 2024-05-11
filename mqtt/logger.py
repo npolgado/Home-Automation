b15868ff296a710ace3b302c4f48d0430ac38b9e
nicholas <nicholas831@gmail.com>
2024-05-10 20:25:44 -0700

    mqtt inital examples

---
+++ mqtt/logger.py
@@ -0,0 +1,35 @@
+import logging
+import paho.mqtt.client as mqtt
+
+logging.basicConfig(level=logging.DEBUG)
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.enable_logger()
+
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+mqttc.loop_start()
+
+def on_log(client, userdata, paho_log_level, messages):
+    if paho_log_level == mqtt.LogLevel.MQTT_LOG_ERR:
+        try: print([message for message in messages])
+        except: print(messages)
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_log = on_log
+
+mqttc.connect("mqtt.eclipseprojects.io", 1883, 60)
+mqttc.loop_start()
+
+"""
+while run:
+    if need_read:
+        mqttc.loop_read()
+    if need_write:
+        mqttc.loop_write()
+    mqttc.loop_misc()
+
+    if not need_read and not need_write:
+        # But don't wait more than few seconds, loop_misc() need to be called regularly
+        wait_for_change_in_need_read_or_write()
+    updated_need_read_and_write()
+"""
